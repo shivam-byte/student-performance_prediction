f3f3dbb7d4b9612cea0fd6cd5091e96498bb920e
shivam <70792906+shivam-byte@users.noreply.github.com>
2025-05-03 17:44:10 +0000

    data transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join('artifacts','train.csv')
@@ -24,10 +24,7 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok = True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index = False,header = True)
-            df['total'] = df['math_score'] + df['reading_score'] + df['writing_score']
-            df['average'] = df['total']/3
-            num = ['math_score','reading_score','writing_score']
-            df = df.drop(num,axis= 1)
+            
             logging.info('train test split initiated')
 
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
@@ -46,4 +43,6 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,11 +1,12 @@
 import sys
 from dataclasses import dataclass
 import pandas as pd 
+import numpy as np
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-
+from src.utils import save_object
 from src.exeption import CustomException
 from src.logger import logging
 import os
@@ -26,7 +27,7 @@ class DataTransformation:
             numerical_features = ['total']
             categorical_features = [
                 'gender',
-                'race_ethinicity',
+                'race_ethnicity',
                 'parental_level_of_education',
                 'lunch',
                 'test_preparation_course',
@@ -35,7 +36,7 @@ class DataTransformation:
             num_pipeline = Pipeline(
                 steps = [
                     ('imputer',SimpleImputer(strategy='mean')),
-                    ('scaler',StandardScaler())
+                    ('scaler',StandardScaler(with_mean=False))
                 ]
             )
 
@@ -43,7 +44,7 @@ class DataTransformation:
               steps = [
                   ('imputer',SimpleImputer(strategy='most_frequent')),
                   ('onehotencder',OneHotEncoder()),
-                  ('scaler',StandardScaler())
+                  ('scaler',StandardScaler(with_mean=False))
               ]    
             )
             logging.info('categorical encoding completed')
@@ -64,5 +65,62 @@ class DataTransformation:
 
 
     def initiate_data_transformation(self,train_path,test_path):
-      train_data = pd.read_csv('artifacts/train.csv')
-      test_data = pd.read_csv("artifacts/test.csv")
+         
+         try:
+             
+            train_data = pd.read_csv(train_path)
+            test_data = pd.read_csv(test_path)
+
+            logging.info('the train and test has been read')
+
+            logging.info('obtaining preprocessor')
+
+            preprocessor_obj = self.get_data_transformer_object()
+
+            #def add_total_and_average(df):
+             #   df['total'] = df['math_score'] + df['reading_score'] + df['writing_score']
+              #  df['average'] = df['total'] / 3
+               # return df.drop(columns=['math_score', 'reading_score', 'writing_score'])
+
+            train_data['total'] = train_data['math_score'] + train_data['reading_score'] + train_data['writing_score']
+            train_data['average'] = train_data['total']/3
+            num = ['math_score','reading_score','writing_score']
+            train_data = train_data.drop(columns=num,axis= 1)
+            
+            test_data['total'] = test_data['math_score'] + test_data['reading_score'] + test_data['writing_score']
+            test_data['average'] = test_data['total']/3
+            num = ['math_score','reading_score','writing_score']
+            test_data = test_data.drop(columns=num,axis= 1)
+            target = 'average'
+            numerical_features = ['total']
+            input_feature_train = train_data.drop(columns=[target],axis = 1)
+            target_feature_train = train_data[target]
+
+            input_feature_test = test_data.drop(columns=[target],axis = 1)
+            target_feature_test = test_data[target]
+            logging.info('applying preprocessing on train and test data')
+            
+            input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train)
+            input_feature_test_arr = preprocessor_obj.transform(input_feature_test)
+            train_arr = np.c_[
+                input_feature_train_arr,np.array(target_feature_train)
+            ]
+
+            test_arr = np.c_[
+                input_feature_test_arr,np.array(target_feature_test)
+
+            ]
+            logging.info('saved preprocessor objects')
+
+            save_object(
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessor_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+         except Exception as e:
+           raise CustomException(e,sys)
